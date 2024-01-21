b85564ce4c809573d3cba3c5a29b81070d1cb16d
Alexandr Meshkov <alex.meh96@mail.ru>
2024-01-21 13:29:53 +0300

    create sign and verify block/transaction

--- core/transaction.go
+++ core/transaction.go
@@ -1,15 +1,37 @@
 package core
 
-import "io"
+import (
+	"fmt"
+	"golang_blockchain_app/crypto"
+)
 
 type Transaction struct {
 	Data []byte
+
+	PublicKey crypto.PublicKey
+	Signature *crypto.Signature
 }
 
-func (tx *Transaction) DecodeBinary(r io.Reader) error {
+func (tx *Transaction) Sign(privetKey crypto.PrivateKey) error {
+	sig, err := privetKey.Sign(tx.Data)
+	if err != nil {
+		return err
+	}
+
+	tx.PublicKey = privetKey.PublicKey()
+	tx.Signature = sig
+
 	return nil
 }
 
-func (tx *Transaction) EncodeBinary(r io.Writer) error {
+func (tx *Transaction) Verify() error {
+	if tx.Signature == nil {
+		return fmt.Errorf("transaction has no signature")
+	}
+
+	if !tx.Signature.Verify(tx.PublicKey, tx.Data) {
+		return fmt.Errorf("invalid transaction signature")
+	}
+
 	return nil
 }
---
+++ core/transaction_test.go
@@ -0,0 +1,32 @@
+package core
+
+import (
+	"github.com/stretchr/testify/assert"
+	"golang_blockchain_app/crypto"
+	"testing"
+)
+
+func TestSignTransaction(t *testing.T) {
+	privateKey := crypto.GeneratePrivateKey()
+	tx := &Transaction{
+		Data: []byte("foo"),
+	}
+
+	assert.Nil(t, tx.Sign(privateKey))
+	assert.NotNil(t, tx.Signature)
+}
+
+func TestVerifyTransaction(t *testing.T) {
+	privateKey := crypto.GeneratePrivateKey()
+	tx := &Transaction{
+		Data: []byte("foo"),
+	}
+
+	assert.Nil(t, tx.Sign(privateKey))
+	assert.Nil(t, tx.Verify())
+
+	otherPrivateKey := crypto.GeneratePrivateKey()
+	tx.PublicKey = otherPrivateKey.PublicKey()
+
+	assert.NotNil(t, tx.Verify())
+}
