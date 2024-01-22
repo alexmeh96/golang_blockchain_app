59f6a3639cfb42274069ae0fb790cd5cfcf1b8cb
Alexandr Meshkov <alex.meh96@mail.ru>
2024-01-22 22:18:52 +0300

    created sort transaction in mempool

--- network/txpool.go
+++ network/txpool.go
@@ -3,7 +3,40 @@ package network
 import (
 	"golang_blockchain_app/core"
 	"golang_blockchain_app/types"
+	"sort"
 )
+
+type TxMapSorted struct {
+	transactions []*core.Transaction
+}
+
+func NewTxMapSorter(txMap map[types.Hash]*core.Transaction) *TxMapSorted {
+	txx := make([]*core.Transaction, len(txMap))
+
+	i := 0
+	for _, val := range txMap {
+		txx[i] = val
+		i++
+	}
+
+	s := &TxMapSorted{txx}
+
+	sort.Sort(s)
+
+	return s
+}
+
+func (s *TxMapSorted) Len() int {
+	return len(s.transactions)
+}
+
+func (s *TxMapSorted) Swap(i, j int) {
+	s.transactions[i], s.transactions[j] = s.transactions[j], s.transactions[i]
+}
+
+func (s *TxMapSorted) Less(i, j int) bool {
+	return s.transactions[i].FirstSeen() < s.transactions[j].FirstSeen()
+}
 
 type TxPool struct {
 	transactions map[types.Hash]*core.Transaction
@@ -13,6 +46,11 @@ func NewTxPool() *TxPool {
 	return &TxPool{
 		transactions: make(map[types.Hash]*core.Transaction),
 	}
+}
+
+func (p *TxPool) Transactions() []*core.Transaction {
+	s := NewTxMapSorter(p.transactions)
+	return s.transactions
 }
 
 func (p *TxPool) Add(tx *core.Transaction) error {
--- network/txpool_test.go
+++ network/txpool_test.go
@@ -3,6 +3,8 @@ package network
 import (
 	"github.com/stretchr/testify/assert"
 	"golang_blockchain_app/core"
+	"math/rand"
+	"strconv"
 	"testing"
 )
 
@@ -22,4 +24,22 @@ func TestTxPoolAddTx(t *testing.T) {
 
 	p.Flush()
 	assert.Equal(t, p.Len(), 0)
+}
+
+func TestSortTransactions(t *testing.T) {
+	p := NewTxPool()
+	txLen := 1000
+
+	for i := 0; i < txLen; i++ {
+		tx := core.NewTransaction([]byte(strconv.FormatInt(int64(i), 10)))
+		tx.SetFirstSeen(int64(i * rand.Intn(10000)))
+		assert.Nil(t, p.Add(tx))
+	}
+
+	assert.Equal(t, txLen, p.Len())
+
+	txx := p.Transactions()
+	for i := 0; i < len(txx)-1; i++ {
+		assert.True(t, txx[i].FirstSeen() < txx[i+1].FirstSeen())
+	}
 }
